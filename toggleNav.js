d69c0768b8771d1acd9f88a639c3f9b39d308492
sethl <lewandowski.seth@gmail.com>
2020-09-30 14:47:22 -0700

    stuff

--- toggleNav.js
+++ toggleNav.js
@@ -8,27 +8,23 @@ function toggleNavigationMenu() {
 
   if (nav.style.height === ""){         // if nav is hidden
   	nav.style.height = "100%";         // open drawer to stated size
-    background.style.opacity = ".3";    // and dim the background
   } else {                              // trigger if function is called and nav is already open 
   	nav.style.height = "";              // make drop down nav go away
-    background.style.opacity = "";      // make opaque background go away
   } 
 }
 
 let lastPosition = window.pageYOffset; // set init value for scroll top
 const nav = document.getElementById('nav-bar'); // get entire nav component
+console.log(window.screen.width);
 // initialize a threshold for hiding nav
-window.addEventListener('scroll', () => {           // when user scrolls, this fires
-  console.log('scroll detected');
-  if (screen.width < 800) {         // run only on mobile device
-    let currentPosition = window.pageYOffset; // set currentPosition
-    
-    console.log(`cp: ${currentPosition} \n lp: ${lastPosition}`)
+  window.addEventListener('scroll', () => {           // when user scrolls, this fires
+  let currentPosition = window.pageYOffset; // set currentPosition
+  console.log(`cp: ${currentPosition} \n lp: ${lastPosition}`)
     if (currentPosition > lastPosition) {         // detect if user has scrolled down
-      nav.style.height = 0;                 // if so, hide entire nav
+      nav.style.height = '0';                 // if so, hide entire nav
     } else if (currentPosition < lastPosition) {  // but if user has scrolled up
-      nav.style.height = "";                     // show nav again
+      nav.style.height = '90px';                     // show nav again
     }
     lastPosition = currentPosition;               // reset value for 'lastPosition';
-  }
 });
+
