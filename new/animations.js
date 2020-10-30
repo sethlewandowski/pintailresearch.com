244fe4447d54631c3fc017748ca5d4dfc9a748d0
sethl <lewandowski.seth@gmail.com>
2020-10-30 11:35:28 -0700

    saving work

--- new/animations.js
+++ new/animations.js
@@ -1,14 +1,22 @@
 "use strict";
 
-const animatedElements = document.querySelectorAll('.animate');
+
+const config = {
+  threshold: 0.5
+};
+
+const animatedElements = document.querySelectorAll('.animate-me');
 
 let observer = new IntersectionObserver((item) => {
   item.forEach(x => {
     if (x.intersectionRatio > 0) {
+      x.target.classList.add('isVisible');
       x.target.classList.add('animate');
-    } 
+    } /*else {
+    	x.target.classList.remove('animate');
+    } */
   });
-});
+}, config);
 
 animatedElements.forEach(x => {
   observer.observe(x);
